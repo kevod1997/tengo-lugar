0943e17eb5b2b31850db2186d274cb7929681d95
Kevin Defalco <kevindefalco@gmail.com>
2025-03-20 00:14:43 -0300

    preparing for first deplay v1.1, too much code to refactor

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,8 +1,10 @@
 import { createAuthClient } from "better-auth/react"
 import { adminClient } from "better-auth/client/plugins"
 
+const baseURL = process.env.BETTER_AUTH_URL || "http://localhost:3000"
+
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000",
+    baseURL,
     plugins: [
         adminClient()
     ]
